012a2cdddbca56fe74b05ce709b4cf0f0d6ea3ff
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-05 15:07:59 +0900

    Initial Commit

---
+++ SampleWeatherSwiftUI/SampleWeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWeatherSwiftUIApp.swift
+//  SampleWeatherSwiftUI
+//
+//  Created by mtanaka on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct SampleWeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
